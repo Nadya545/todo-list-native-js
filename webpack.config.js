ed4c5f8cb0758ed5d835dffd3a6c698220740d23
Nadya545 <nadyamishina48@gmail.com>
2025-01-18 18:49:11 +0300

    доделка

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
-
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -9,11 +11,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // Преобразует CSS в CommonJS
+          "sass-loader",
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Укажите путь к вашему HTML файлу
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/css.css", to: "css.css" }],
+    }),
+  ],
   mode: "development", // или 'production'
   devServer: {
     static: path.resolve(__dirname, "dist"),
